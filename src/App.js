a0e6c31177c0cd17de0e6b4ec936c5b5868cda22
Amandozz <amanda.srisou@gmail.com>
2021-08-24 17:23:12 +1000

    add performances details icons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
 import Header from "./Profile/Header";
 import Sidebar from "./Profile/Sidebar";
+import calories from "./Assets/Icons/calories.png";
+import protein from "./Assets/Icons/protein.png";
+import carbs from "./Assets/Icons/carbs.png";
+import fat from "./Assets/Icons/fat.png";
 
 function App() {
   return (
@@ -9,7 +13,44 @@ function App() {
       <Sidebar />
 
       <div className="main">
-          <p>Bonjour Thomas</p>
+          <h1>Bonjour <span className="name">Thomas</span></h1>
+          <p>Félicitation ! Vous avez explosé vos objectifs hier 👏</p>
+
+          <div className="container">
+              <div>
+                  <div className="activity">
+                  </div>
+
+                  <div className="container-performances">
+                      <div className="performances">
+                      </div>
+
+                      <div className="performances">
+                      </div>
+
+                      <div className="performances">
+                      </div>
+                  </div>
+              </div>
+
+              <div className="container-performances-detail">
+                  <div className="performances-details">
+                      <img className="performances-details-icon" src={calories} alt="calories-icon" />
+                  </div>
+
+                  <div className="performances-details">
+                      <img className="performances-details-icon" src={protein} alt="protein-icon"/>
+                  </div>
+
+                  <div className="performances-details">
+                      <img className="performances-details-icon" src={carbs} alt="carbs-icon"/>
+                  </div>
+
+                  <div className="performances-details">
+                      <img className="performances-details-icon" src={fat} alt="fat-icon"/>
+                  </div>
+              </div>
+          </div>
       </div>
 
     </div>
