ef1b0e6cbe8ef2f5d70ddd03b39802f33e6f9492
Amandozz <amanda.srisou@gmail.com>
2021-09-23 17:24:00 +1000

    add router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import './App.css';
-import Header from "./Components/Header";
-import Sidebar from "./Components/Sidebar";
+import React from "react";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+} from "react-router-dom";
 import Profile from "./Components/Profile";
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Sidebar />
-      <Profile />
-    </div>
+      <Router>
+          <Switch>
+              <Route exact path="/user/:id">
+                  <Profile />
+              </Route>
+          </Switch>
+      </Router>
   );
 }
 
